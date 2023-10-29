478cf4a47e16aca7a0bb48e8103d173222f4c62d
Vladislav Forsh <vlad@robowhale.com>
2023-10-29 13:29:39 +0300

    update pm2 config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -11,7 +11,7 @@ module.exports = {
       'repo': 'git@github.com:vforsh/release-o-matic.git',
       'path': '/var/www/html/papa-cherry-2/releases',
       'post-setup': '/root/.bun/bin/bun install',
-      'post-deploy': '/usr/bin/bash -c "pm2 startOrRestart ecosystem.config.js"',
+      'post-deploy': '/usr/bin/bash -c \"pm2 startOrRestart ecosystem.config.js\"',
     },
   },
 }
