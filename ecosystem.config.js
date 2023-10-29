c86a603eecdda0e21ad47b5098df5718d851e5d2
Vladislav Forsh <vlad@robowhale.com>
2023-10-29 13:15:51 +0300

    update pm2 config

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  name: 'release-o-matic',
+  script: 'src/index.ts',
+  interpreter: '/Users/vlad/.bun/bin/bun',
+
+  deploy: {
+    production: {
+      'user': 'root',
+      'host': ['robowhale.co'],
+      'ref': 'origin/master',
+      'repo': 'git@github.com:vforsh/release-o-matic.git',
+      'path': '/var/www/html/papa-cherry-2/releases',
+      'post-setup': '/root/.bun/bin/bun install',
+      'post-deploy': 'pm2 startOrRestart --env production',
+    },
+  },
+}
