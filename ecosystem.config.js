d9400851542e74816d6ba7b109681ba242c027fd
Vladislav Forsh <vlad@robowhale.com>
2023-10-31 15:13:34 +0300

    use Elysia instead of Hono

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -10,8 +10,8 @@ module.exports = {
       'ref': 'origin/master',
       'repo': 'git@github.com:vforsh/release-o-matic.git',
       'path': '/var/www/html/papa-cherry-2/releases',
-      'post-setup': '/root/.bun/bin/bun install',
-      'post-deploy': 'pm2 startOrRestart ecosystem.config.js;',
+      // 'post-setup': '',
+      'post-deploy': 'bun install; pm2 startOrRestart ecosystem.config.js;',
     },
   },
 }
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,8 @@
-import { Hono } from 'hono'
+import { Elysia, t } from 'elysia'
+import __Dirname from 'tiny-dirname'
+import * as path from 'path'
+
+const __dirname = __Dirname(import.meta.url)
 
 type ReleaseInfo = {
 	current: boolean
@@ -8,12 +12,16 @@ type ReleaseInfo = {
 	releasedAt: number,
 }
 
-const app = new Hono()
+const gameRoot = process.env.NODE_ENV === 'development' ? path.join(__dirname, '../test/papa-cherry-2') : path.join(__dirname, '../../../')
 
-app.get('/', (c) => c.text('Hello Hono!'))
+const app = new Elysia()
+
+app.get('/', (ctx) => ctx.set.redirect = '/list')
+
+app.get('/game', (ctx) => gameRoot)
 
 app.get('/list', (ctx) => {
-	return ctx.json([{
+	return [{
 		current: true,
 		key: 'master-11',
 		index: 'index_master-11.html',
@@ -25,7 +33,39 @@ app.get('/list', (ctx) => {
 		index: 'index_master-10.html',
 		files: 'files_master-10.json',
 		releasedAt: Date.now() - 10_000,
-	}] as ReleaseInfo[])
+	}] as ReleaseInfo[]
 })
+
+app.get('/current', (ctx) => {
+	console.log(__dirname)
+	
+	return {
+		current: true,
+		key: 'master-11',
+		index: 'index_master-11.html',
+		files: 'files_master-11.json',
+		releasedAt: Date.now(),
+	}
+})
+
+app.post('/publish', (ctx) => ctx.query, {
+	query: t.Object({
+		platform: t.String({ format: 'uri' }),
+		env: t.Optional(t.String()),
+	}),
+})
+
+app.post('/rollback', (ctx) => ctx.body, {
+	body: t.Object({
+		platform: t.String({ format: 'uri' }),
+		build: t.Optional(t.String({ pattern: '(\w+)-(\d+)' })),
+	}),
+})
+
+app.listen(3000)
+
+console.log(
+	`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`,
+)
 
 export default app
