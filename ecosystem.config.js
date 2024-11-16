3b2eda2a5f71290f022671a519d461b794aec22e
Vladislav Forsh <vlad@robowhale.com>
2024-11-16 11:30:51 +0300

    move secrets to .env

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,6 +1,12 @@
 const BUN_DIR = '/root/.bun/bin'
 const BUN = `${BUN_DIR}/bun`
 
+const HOST = `robowhale.ru`
+const DEPLOY_DIR = `/var/www/html/papa-cherry-2/releases`
+
+/**
+ * @link https://pm2.keymetrics.io/docs/usage/deployment/
+ */
 module.exports = {
   name: 'release-o-matic',
   script: 'src/index.ts',
@@ -9,10 +15,11 @@ module.exports = {
   deploy: {
     production: {
       'user': 'root',
-      'host': ['robowhale.ru'],
+      'host': [HOST],
       'ref': 'origin/master',
       'repo': 'git@github.com:vforsh/release-o-matic.git',
-      'path': '/var/www/html/papa-cherry-2/releases',
+      'path': DEPLOY_DIR,
+      "post-setup": `scp .env.prod root@${HOST}:${DEPLOY_DIR}/source`,
       'post-deploy': `${BUN} install && ${BUN_DIR}/pm2 startOrRestart ecosystem.config.js`,
     },
   },
